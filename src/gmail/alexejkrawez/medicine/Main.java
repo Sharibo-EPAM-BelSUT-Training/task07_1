12af9cc483bf3687ab5d7fb76d31dab64f57f2e8
Sharibo <AlexejKrawez@gmail.com>
2021-07-22 20:33:13 +0300

    style|fix: add description to Main.java, change encoding Java.SE.07.Java XML.txt to UTF-8

--- src/gmail/alexejkrawez/medicine/Main.java
+++ src/gmail/alexejkrawez/medicine/Main.java
@@ -8,6 +8,16 @@ import javax.xml.bind.*;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 
+/**
+ * @name Java.SE.07-1
+ * @package gmail.alexejkrawez.medicine;
+ * @file Main.java
+ * @author Alexej Krawez
+ * @email AlexejKrawez@gmail.com
+ * @created 17.01.2019
+ * @updated 22.07.2021
+ * @version 1.1
+ */
 public class Main {
 
     public static void main(String[] args) {
